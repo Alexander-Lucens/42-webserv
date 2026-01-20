54fad1f2383ce2043f2077860364871f10e60ca0
AntoineMargo <antoine.mrg@gmail.com>
2026-01-20 13:49:15 +0100

    feat: Connection hpp

---
+++ includes/Connection.hpp
@@ -0,0 +1,26 @@
+
+#pragma once
+
+#include <string>
+#include <map>
+#include <iostream>
+#include <sstream>
+
+#include "Request.hpp"
+
+
+class Connection {
+	public:
+		std::string socket;
+		std::string read_buffer;
+		std::string write_buffer;
+		int			parse_state;
+		Request		request;
+		Connection();
+		~Connection();
+		Connection(const Connection &other);
+		Connection& operator=(const Connection &other);
+};
+
+
+// void client_connection (Connection client&, char* data&, int size);
